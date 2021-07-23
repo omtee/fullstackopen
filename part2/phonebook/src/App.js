9c0a5a776c070fe4a2c00a1984904e75195af8a7
Olli Tikka <olli.tikka@andritz.com>
2021-07-23 12:44:02 +0300

    part2 phonebook db

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Number = ({ person }) => (
   <p>{person.name} {person.number}</p>
@@ -36,15 +37,18 @@ const Persons = ({ persons }) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -58,6 +62,7 @@ const App = () => {
     const PersonObject = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     }
 
     setPersons(persons.concat(PersonObject))
