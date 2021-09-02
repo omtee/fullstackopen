ac3fda80a286fb611286656eca94abbe97c93a56
Olli Tikka <olli.tikka@andritz.com>
2021-09-02 15:39:58 +0300

    part8-fragment queries

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -1,56 +1,66 @@
 import { gql } from '@apollo/client'
 
-export const ALL_AUTHORS = gql`
-query getAllAuthors {
-  allAuthors  {
-    name
-    born
-    id
-    bookCount
-  }
-}
-`
-
-export const ALL_BOOKS = gql`
-query getAllBooks($genreToSearch: String) {
-  allBooks(genre: $genreToSearch)  {
-    title
-    author { name }
-    published
-    genres
-    id
-  }
-}
-`
-
-export const ADD_BOOK = gql`
-mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
-  addBook(
-    title: $title,
-    author: $author,
-    published: $published,
-    genres: $genres
-  ) {
+const BOOK_DETAILS = gql`
+  fragment BookDetails on Book {
     title
     author { name born bookCount id }
     published
     genres
     id
   }
-}
 `
 
-export const EDIT_AUTHOR = gql`
-mutation editAuthor($name: String!, $setBornTo: Int!) {
-  editAuthor(
-    name: $name,
-    setBornTo: $setBornTo
-  ) {
+const AUTHOR_DETAILS = gql`
+  fragment AuthorDetails on Author {
     name
     born
     id
+    bookCount
   }
-}
+`
+
+export const ALL_AUTHORS = gql`
+  query getAllAuthors {
+    allAuthors {
+      ...AuthorDetails
+    }
+  }
+  ${AUTHOR_DETAILS}
+`
+
+export const EDIT_AUTHOR = gql`
+  mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(
+      name: $name,
+      setBornTo: $setBornTo
+    ) {
+      ...AuthorDetails
+    }
+  }
+  ${AUTHOR_DETAILS}
+`
+
+export const ALL_BOOKS = gql`
+  query getAllBooks($genreToSearch: String) {
+    allBooks(genre: $genreToSearch) {
+      ...BookDetails
+    }
+  }
+  ${BOOK_DETAILS}
+`
+
+export const ADD_BOOK = gql`
+  mutation addBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(
+      title: $title,
+      author: $author,
+      published: $published,
+      genres: $genres
+    ) {
+      ...BookDetails
+    }
+    ${BOOK_DETAILS}
+  }
 `
 
 export const LOGIN = gql`
